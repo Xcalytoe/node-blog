722c1e42fa40c316673ce6da78cba00bcaa46f79
Ezejiofor Cosmas <ezejioforcosmas@gmail.com>
2023-03-14 14:22:25 +0100

    feat: create ejs files

--- app.js
+++ app.js
@@ -9,9 +9,14 @@ app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
-
+app.get("about-us", (req, res) => {
+  res.render("about");
+});
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.redirect("/about-us");
+});
+app.use((req, res) => {
+  res.status(404).render("404");
 });
